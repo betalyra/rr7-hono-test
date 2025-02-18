2a93b6e1ab264aa1f69b61a21ef7bfabdaf320f4
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2025-02-18 18:39:58 +0000

    Next

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { createRequestHandler } from "react-router";
+import { createHonoServer } from "react-router-hono-server/node";
 
 // @ts-expect-error - virtual module provided by React Router at build time
 import * as build from "virtual:react-router/server-build";
@@ -10,16 +11,24 @@ declare module "react-router" {
   }
 }
 
-const app = new Hono();
+// const app = new Hono();
 
 // Add any additional Hono middleware here
 
-const handler = createRequestHandler(build);
-app.mount("/", (req: Request) =>
-  handler(req, {
-    // Add your "load context" here based on the current request
-    VALUE_FROM_HONO: "Hello from Hono",
-  })
-);
+// const handler = createRequestHandler(build);
+// app.mount("/", (req: Request) =>
+//   handler(req, {
+//     // Add your "load context" here based on the current request
+//     VALUE_FROM_HONO: "Hello from Hono",
+//   })
+// );
+const app = await createHonoServer({
+  getLoadContext(_, { build, mode }) {
+    const isProductionMode = mode === "production";
+    return {
+      VALUE_FROM_HONO: isProductionMode ? build.assets.version : "dev",
+    };
+  },
+});
 
 export default app.fetch;
