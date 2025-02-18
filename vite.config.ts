2cb38efe3aa866c155c5d900ecd1955217f81538
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2025-02-18 18:42:07 +0000

    Next

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig(({ command, isSsrBuild }) => ({
   build: {
     rollupOptions: isSsrBuild
@@ -14,5 +14,5 @@ export default defineConfig(({ command, isSsrBuild }) => ({
   ssr: {
     noExternal: command === "build" ? true : undefined,
   },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), topLevelAwait()],
 }));
