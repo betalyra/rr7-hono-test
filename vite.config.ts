93ccd024aa78ca3b18e68e8d84b966c7a924fa1e
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2025-02-18 18:44:29 +0000

    Next

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
 export default defineConfig(({ command, isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./server/app.ts",
-        }
-      : undefined,
-  },
-  ssr: {
-    noExternal: command === "build" ? true : undefined,
-  },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), topLevelAwait()],
+  // build: {
+  //   rollupOptions: isSsrBuild
+  //     ? {
+  //         input: "./server/app.ts",
+  //       }
+  //     : undefined,
+  // },
+  // ssr: {
+  //   noExternal: command === "build" ? true : undefined,
+  // },
+  plugins: [
+    tailwindcss(),
+    reactRouterHonoServer({ runtime: "aws", dev: { export: "handler" } }), // add this
+    reactRouter(),
+    tsconfigPaths(),
+    topLevelAwait(),
+  ],
 }));
